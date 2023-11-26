31cceff76a7a068c0201b791d90924e78f759193
VoAnhDuy2702 <112914958+VoAnhDuy2702@users.noreply.github.com>
2023-11-26 09:09:49 +0700

    first commit

--- progress-report/da20tta_110120167_voanhduy.py
+++ progress-report/da20tta_110120167_voanhduy.py
@@ -38,27 +38,3 @@ tulips = list(data_dir.glob('tulips/*'))
 PIL.Image.open(str(tulips[0]))
 
 PIL.Image.open(str(tulips[1]))
-"""## Tạo tập dữ liệu"""
-
-batch_size = 32
-img_height = 180
-img_width = 180
-
-train_ds = tf.keras.utils.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
-
-val_ds = tf.keras.utils.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="validation",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
-
-class_names = train_ds.class_names
-print(class_names)
